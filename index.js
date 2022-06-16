cd307040825670a11625b6ac8dcc1424c7a9f408
Marisa <marisae05@gmail.com>
2022-06-16 11:48:44 -0700

    Done with assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+function add() {
+    // we'll fill this in shortly
+}
+function subtract() {
+    // to be filled in later
+}
+function multiply() {
+    // will fill in shortly
+}
+function divide () {
+    // will fill in shortly
+}
+function add(a, b) {
+    return a +b;
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+
+
+function increment(a) {
+    return a += 1
+}
+
+function decrement(a) {
+    return a -= 1
+}
+
+
+function makeInt(a) {
+    return parseInt(a, 10)
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
